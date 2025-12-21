287f417fdd6ed55358dffe4d917a06ac872b9953
Jeffrey Grizzle <jeffgrizzle77@gmail.com>
2025-12-21 11:43:46 -0500

    add auth based tables and seeding

--- db/seed.ts
+++ db/seed.ts
@@ -4,6 +4,15 @@ import prisma from "../lib/prisma";
 async function main() {
   await prisma.product.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
+
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  await prisma.user.createMany({ data: sampleData.users });
+
+  console.log("Database seeding successful");
 }
 
 main()
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter });
+  return new PrismaClient({ adapter }).$extends(withAccelerate());
 };
 
 declare const globalThis: {
